a533ba4c03ba362ec443f56260c79a01312b5176
Skyline141 <otmorben@gmail.com>
2025-11-05 18:46:05 +0700

    Day 2: input, math operations

---
+++ python-journey.py
@@ -0,0 +1,4 @@
+number = int(input('Введите число: '))
+print(f'Квадрат вашего числа:  {number * number}')
+print(f'Ваше удвоенное число:  {number * 2}')
+print(f'Остаток вашего числа деленное на 3: {number % 3}')
